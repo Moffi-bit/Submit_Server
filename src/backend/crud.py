e4ca1c9cc01f5cde71872600a9f0c484347de387
Moffi-bit <lorenzostiavelli6@gmail.com>
2023-02-24 23:53:22 -0500

    Tested some endpoints

--- src/backend/crud.py
+++ src/backend/crud.py
@@ -1,13 +1,34 @@
 import requests
 
-data_json = {
-    "first": "john",
-    "last": "larry",
-    "email": "yessir@gmail",
-    "user": "geek",
-    "pass": "coding",
-}
+def post_user():
+    user_json = {
+        "first": "john",
+        "last": "larry",
+        "email": "yessir@gmail",
+        "user": "geek",
+        "pass": "coding",
+    }
 
-x = requests.post('http://127.0.0.1:5000/login/user/', data=data_json)
+    user_id = requests.post('http://127.0.0.1:5000/login/user/', data=user_json)
 
-print(x.text)
+    return user_id.text
+
+def post_class():
+    user_json = {
+        "name": "math400",
+    }
+
+    class_id = requests.post('http://127.0.0.1:5000/login/class/', data=user_json)
+
+    return class_id.text
+
+def get_user(id):
+    return requests.get(f'http://127.0.0.1:5000/login/user/{id}').text
+
+def main():
+    id_of_new_user = post_user()
+    id_of_new_class = post_class()
+    print(get_user(id_of_new_user))
+
+if __name__ == "__main__":
+    main()
