c27870e7b2c869d8c0fcdd4749c6b1c2d00d5c70
AdityaShelke29 <adityashelke2014@gmail.com>
2023-03-04 12:02:11 -0500

    added basic login page

--- src/frontend/components/Login.jsx
+++ src/frontend/components/Login.jsx
@@ -1,19 +1,58 @@
 import React from 'react'
 import image from '../../assets/login-left-modal.png'
 
+import '../styling/Login.css'
+
 /* Styling Imports */
 
 const Login = () => {
   return (
     <div className = "window-login">
-        <div className = "overall-modal">
-          <div className = "modal-left">
-            <img src = {image}></img>
-          </div>
-          <div className = "modal-right">
+      <div className = "window-header">
 
+      </div>
+
+      <div className = "window-main">
+        <div className = "overall-modal">
+            <div className = "modal-left">
+              <div className = "modal-left-top">
+
+                <p className = "text1">The world's best computer science grading tool. </p>
+
+              </div>
+
+              <div className = "modal-left-bottom">
+                <img className = "image1" src = {image}></img>
+              </div>
+
+            </div>
+            <div className = "modal-right">
+              <p className = "text2">Sign Up</p>
+              <p className = "text3">Have an account already? </p>
+
+              <p className = "input-label">I am a...</p>
+              <div className = "user-select-outer">
+                <button className = "user-select-button" >Student</button>
+                <button className = "user-select-button" >Teacher</button>
+                <button className = "user-select-button" >TA</button>
+              </div>
+
+              <form className = "sign-up-form">
+                <label className = "input-label" for="fname">Full Name</label>
+                <input className = "input-field" type="text" id="fname" placeholder = "Enter full name"/>
+
+                <label className = "input-label" for="lname">Email</label>
+                <input className = "input-field" type="text" id="lname" placeholder = "Enter email"/>
+
+                <label className = "input-label" for="password">Password</label>
+                <input className = "input-field" type="password" id="password" placeholder = "Enter password"/>
+
+                <input className = "input-button" type="submit" value="Create Account"/>
+              </form>
+              
           </div>
         </div>
+      </div>
     </div>
   )
 }
